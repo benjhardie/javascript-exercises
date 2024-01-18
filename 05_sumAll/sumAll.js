65ce681fe1d1897b9270bfedc415666dafe01415
Ben <benjhardie@icloud.com>
2024-01-18 22:35:11 +0000

    feat: add sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    if (typeof x !== "number" || typeof y !== "number") {
+        return "ERROR";
+    } else if (x < 0 || y < 0) {
+        return "ERROR";
+    }
 
+    const rangeStart = x < y ? x : y;
+    const rangeEnd = x > y ? x : y;
+
+    let sum = 0;
+    for (let num = rangeStart; num <= rangeEnd; num++) {
+        sum += num;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
