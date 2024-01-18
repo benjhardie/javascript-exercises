5e7576faab6821af0e2f1da8e7a235daf8c1c410
Ben <benjhardie@icloud.com>
2024-01-18 22:04:19 +0000

    feat: add removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
 
+    for (value of values) {
+        for (let index = 0; index <= array.length - 1; index++) {
+            if (array[index] === value) {
+                array.splice(index, 1);
+                break;
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
