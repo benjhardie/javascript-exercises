905b5f38e3b09a6a0c3f9dff711119801b07d6fe
Ben <benjhardie@icloud.com>
2024-01-18 20:12:13 +0000

    feat: add reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    arrString = string.split("");
+    arrReverse = [];
+    for (char of arrString) {
+        arrReverse.unshift(char);
+    }
+    return arrReverse.join("");
 };
 
 // Do not edit below this line
