06a6481b0cb3b3c17ca87c1b98a2721715e1c2e0
Ben <benjhardie@icloud.com>
2024-02-06 20:10:23 +0000

    feat: code solution to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    const currentDate = new Date();
 
+    array.sort((a, b) => {
+        const previous = a.yearOfDeath
+            ? a.yearOfDeath - a.yearOfBirth
+            : currentDate.getFullYear() - a.yearOfBirth;
+        const next = b.yearOfDeath
+            ? b.yearOfDeath - b.yearOfBirth
+            : currentDate.getFullYear() - b.yearOfBirth;
+        return previous > next ? -1 : 1;
+    });
+
+    return array[0];
 };
 
 // Do not edit below this line
