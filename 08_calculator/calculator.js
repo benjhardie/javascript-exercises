d521aa19582b58fcfd97391d6421b7f33489398a
Ben <benjhardie@icloud.com>
2024-02-06 19:44:25 +0000

    feat: code solution to calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,36 @@
-const add = function() {
-	
+const add = function (x, y) {
+    return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+    return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+    return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+    return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorial(num - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
