8e1c9ce0115b032e29e6f13275ad93f585fe38bb
Ben <benjhardie@icloud.com>
2024-02-06 20:07:05 +0000

    feat: code solution to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = (function () {
+    let memo = [0, 1];
+    let fib = function (n) {
+        if (n < 0) {
+            return "OOPS"
+        }
+        
+        let result = memo[n];
+        if (typeof result !== "number") {
+            result = fib(n - 1) + fib(n - 2);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return fib;
+})();
 
 // Do not edit below this line
 module.exports = fibonacci;
